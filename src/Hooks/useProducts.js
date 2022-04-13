d5c6f4d09f161468cb815acc2f3c7144cd9913e2
AhshanHabib26 <habib.net27@gmail.com>
2022-04-13 08:26:55 +0600

    React Router Setup

--- src/Hooks/useProducts.js
+++ src/Hooks/useProducts.js
@@ -1,21 +1,15 @@
 import { useEffect, useState } from "react";
 
-
 const useProducts = () => {
 
     const [products, setProducts] = useState([])
     useEffect( () =>{
         fetch('data.json')
         .then(res => res.json())
-        .then()
+        .then(data => setProducts(data))
     }, [])
 
-
-    return (
-        <div>
-            
-        </div>
-    );
+    return [products, setProducts]
 };
 
 export default useProducts;
